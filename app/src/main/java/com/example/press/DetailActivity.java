74b364a908dcbddb7c62436e54c8b1e3c5bb3868
hanum <rntls123@naver.com>
2021-11-26 21:00:10 +0900

    11.26 Register RecordActivity on manifest

--- app/src/main/java/com/example/press/DetailActivity.java
+++ app/src/main/java/com/example/press/DetailActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
--- app/src/main/java/com/example/press/RecordActivity.java
+++ app/src/main/java/com/example/press/RecordActivity.java
@@ -1,4 +1,23 @@
 package com.example.press;
 
-public class RecordActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RecordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+        // Intent 정보 수신
+        Intent intent = getIntent();
+        String exercise = intent.getExtras().getString("exercise");
+
+        TextView subtitle = (TextView) findViewById(R.id.subtitle);
+        subtitle.setText(exercise);
+    }
+
 }
