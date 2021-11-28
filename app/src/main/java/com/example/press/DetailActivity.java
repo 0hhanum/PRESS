bd9e58875e410b1a4b08579eeaa22381f530c39a
hanum <rntls123@naver.com>
2021-11-28 23:41:35 +0900

    11.28 Get total volume by date using SQL GROUP BY

--- app/src/main/java/com/example/press/DetailActivity.java
+++ app/src/main/java/com/example/press/DetailActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -12,6 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
+    DBHelper dbHelper = new DBHelper(DetailActivity.this, 1);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +26,17 @@ public class DetailActivity extends AppCompatActivity {
         TextView subtitle = (TextView) findViewById(R.id.subtitle);
         subtitle.setText(exercise);
 
-//        List<Object> sets =
+        List<Object> sets = dbHelper.getResultByDate(exercise);
+        for (int i=0;i<sets.size();i += 2){
+//            String date = String.valueOf(sets.get(i));
+//            int total =
+            Log.d("testing", String.valueOf(sets.get(i)));
+            Log.d("testing", String.valueOf(sets.get(i+1)));
+        }
     }
     public void go_to_exercise(View view){
         Intent intent = new Intent(DetailActivity.this, RecordActivity.class);
         intent.putExtra("exercise", getIntent().getExtras().getString("exercise"));
         startActivity(intent);
     }
-
 }
